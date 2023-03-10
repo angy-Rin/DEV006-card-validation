ddf4673f0ed4959ab443ae94454c6771a421f046
Angélica Rincon <rinconmezangelica@gmail.com>
2023-03-10 16:24:45 -0600

    prueba de borrado y agregado de mismo archivo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,208 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const ceros= "0000 0000 0000 0000";
+const cero="00";
+const cvc="123";
+
+const boton1 = document.getElementById("boton");
+const boton2 = document.getElementById("boton2");
+const segundapantalla = document.getElementById("segunda-pantalla");
+const datosprimerapantalla = document.getElementById("datosgenerales");
+
+const liveNombre = document.getElementById("live-nombre")
+const liveNumero = document.getElementById("live-tarjeta")
+const liveMes = document.getElementById("live-mes")
+const liveYear = document.getElementById("live-año")
+const liveCvc = document.getElementById("live-cvc")
+
+const cajaNombre = document.getElementById("nombre-tarjeta")
+const cajaNumero = document.getElementById("numero-tarjeta")
+const cajaMes = document.getElementById("mes")
+const cajaYear = document.getElementById("year")
+const cajaCvc = document.getElementById("cvc")
+
+const errorNombre = document.getElementById("errornombre")
+const errorNumero = document.getElementById("errornumero")
+const errorYear = document.getElementById("erroraño")
+const errorMes = document.getElementById("errormes")
+const errorCvc = document.getElementById("errorcvc")
+
+cajaNombre.onkeyup = function() {myFunction1()};
+cajaNumero.onkeyup = function() {myFunction2()};
+cajaMes.onkeyup = function() {myFunction3()};
+cajaYear.onkeyup = function() {myFunction4()};
+cajaCvc.onkeyup = function() {myFunction5()};
+
+
+boton1.addEventListener("click", Confirmacion);
+
+segundapantalla.style.display="none";
+
+cajaNumero.addEventListener('keyup', (e) =>{
+  let valorInput = e.target.value;
+  cajaNumero.value = valorInput
+  //Eliminar espacio de texto, lo reemplaza por nada
+  .replace(/\s/g, "")
+   //espaciado cada 4 numeros
+  .replace(/([0-9]{4})/g, "$1 ")
+ 
+  /*.replace(valorInput,function () {
+                   let i=0;
+                    let x = cajaNumero.value.split("");
+                    while (i < x.length - 4) {
+                    x[i] = "#";
+                    i++;} 
+                    return x.join("");
+                  })
+  */
+  //quitar ultimo espacio
+  .trim();
+}
+)
+
+
+function myFunction1() {
+  liveNombre.innerHTML=cajaNombre.value;
+  if (!cajaNombre.value){
+    liveNombre.innerHTML = cajaNombre.placeholder;
+  }
+}
+
+function myFunction2() {
+  const creditCardNumber=cajaNumero.value;
+  let escondido= validator.Maskify(creditCardNumber);
+  liveNumero.innerHTML = escondido;
+ 
+  if (!cajaNumero.value){
+    liveNumero.innerHTML = ceros;
+  }
+}
+
+function myFunction3() {
+  liveMes.innerHTML = cajaMes.value;
+  if (!cajaMes.value){
+    liveMes.innerHTML = cero;
+  }
+}
+
+function myFunction4() {
+  liveYear.innerHTML = cajaYear.value;
+  if (!cajaYear.value){
+    liveYear.innerHTML = cero;
+  }
+}
+
+function myFunction5() {
+  liveCvc.innerHTML = cajaCvc.value;
+  if (!cajaCvc.value){
+    liveCvc.innerHTML = cvc;
+  }
+}
+
+function Confirmacion(){
+  const regExp = /[a-zA-Z]/g;
+  const mensaje="Solo se aceptan numeros";
+  const mensaje2="Dato en blanco";
+  const mensaje3="Solo se aceptan letras";
+
+  let rb = 0;
+  let ra = 0;
+  let rc = 0;
+  let rd = 0;
+  let re = 0;
+  let rf = 0;
+
+  const colorojo= "1px solid hsl(0, 100%, 66%)";
+
+  if ((/\d+/g).test(cajaNombre.value)) {
+    errorNombre.innerHTML = mensaje3;
+    cajaNombre.style.border = colorojo;
+  } else if (!cajaNombre.value){
+    errorNombre.innerHTML = mensaje2;
+    cajaNombre.style.border = colorojo;
+  } else{
+    errorNombre.innerHTML = " ";
+    cajaNombre.style.border = null;
+    ra=1;
+  }
+
+  if (regExp.test(cajaNumero.value)) {
+    errorNumero.innerHTML = mensaje;
+    cajaNumero.style.border = colorojo;
+  } else if (cajaNumero.value.length>0 && cajaNumero.value.length<19){
+    errorNumero.innerHTML = "Faltan digitos";
+    cajaNumero.style.border = colorojo;
+  } else if (!cajaNumero.value) {
+    errorNumero.innerHTML = mensaje2;
+    cajaNumero.style.border = colorojo;
+  } else{
+    errorNumero.innerHTML = " ";
+    cajaNumero.style.border = null;
+    rb=1;
+  }
+
+  if (regExp.test(cajaMes.value) ) {
+    errorMes.innerHTML = mensaje;
+    cajaMes.style.border = colorojo;
+  } else if (cajaMes.value>12){
+    errorMes.innerHTML = "Mes incorrecto";
+  } else if (!cajaMes.value){
+    errorMes.innerHTML = mensaje2;
+    cajaMes.style.border = colorojo;
+  } else{
+    errorMes.innerHTML = " ";
+    cajaMes.style.border = null;
+    rc=1;
+  }
+
+  if (regExp.test(cajaYear.value)) {
+    errorYear.innerHTML = mensaje;
+    cajaYear.style.border = colorojo;
+  } else if (cajaYear.value<23 && cajaYear.value>0) {
+    errorYear.innerHTML = "Año expirado";
+    cajaYear.style.border = colorojo;
+  } else if (!cajaYear.value){
+    errorYear.innerHTML = mensaje2;
+    cajaYear.style.border = colorojo;
+  } else{
+    errorYear.innerHTML = " ";
+    cajaYear.style.border = null;
+    rd=1;
+  }
+  
+  if (regExp.test(cajaCvc.value)) {
+    errorCvc.innerHTML = mensaje;
+    cajaCvc.style.border = colorojo;
+  } else if (cajaCvc.value.length>0 && cajaCvc.value.length<3){
+    errorCvc.innerHTML = "Faltan digitos";
+    cajaCvc.style.border = colorojo;
+  } else if (!cajaCvc.value){
+    errorCvc.innerHTML = mensaje2;
+    cajaCvc.style.border = colorojo;
+  } else{
+    errorCvc.innerHTML = " "; 
+    cajaCvc.style.border = null;
+    re=1;
+  }
+  const creditCardNumber=cajaNumero.value;
+  const valido=validator.IsValid(creditCardNumber)
+
+  if ( valido === true){
+    rf=1;
+  }else{
+    errorCvc.innerHTML = "ERROR";
+  }
+
+  if (ra==1 && rb==1 && rc==1 && re==1 && rd==1 && rf==1){
+    Segundapantalla();
+  }
+}
+
+function Segundapantalla() {
+  segundapantalla.style.display="block";
+  datosprimerapantalla.style.display="none";
+}
+
+
