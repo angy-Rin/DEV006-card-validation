84f3ba9f5aa778644ea992cd0e8ccb96a0de686b
Angélica Rincon <rinconmezangelica@gmail.com>
2023-03-10 16:21:20 -0600

    primera propuesta funcional de objeto validator

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
+
 const validator = {
-  // ...
+    Maskify: function(creditCardNumber) {
+        let x=creditCardNumber.replace(/\s/g, '').split("");
+        let i=0;
+        while (i < x.length - 4) {
+            x[i] = "#";
+            i++;
+        } 
+        return x.join("");
+        },
+    
+    IsValid: function(creditCardNumber){
+        let y=0;
+        let x=creditCardNumber.replace(/\s/g, '').split("").reverse();
+        let suma=0;
+        let suma2=0;
+        for (let i = 0; i < x.length; i++) {
+        if (i % 2 === 1) {
+            x[i] = x[i] * 2;
+            y=x[i].toString().split("");
+            for (let i=0; i<y.length;i++){
+                suma += parseInt(y[i],10);
+            }
+        } else{
+            suma2 += parseInt(x[i],10)
+        }
+        }
+        let sumatotal= suma+suma2;
+        if (sumatotal % 10 === 0) {
+        return true;
+        } else {
+        return false;
+        } 
+    }
+    
 };
 
 export default validator;
+
+
